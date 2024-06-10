43267422f8382b920d7ea22699c9d1c6e4c58fe8
Szymon Taranczewski <sz.taranczewski@gmail.com>
2024-06-10 12:32:27 +0200

    finishing cover section and trying to implement featured posts section

---
+++ src/components/blog/blogLayoutOne.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Tag from "@/components/elements/tag";
+import Link from "next/link";
+import Image from 'next/image'
+
+
+const BlogLayoutOne = ({ blog }) => {
+  return (
+    <div className="inline-block overflow-hidden rounded-xl">
+      <div className="rounded-3x1 absolute bottom-0 left-0 right-0 top-0 -z-10 h-full bg-gradient-to-b from-transparent from-0% to-dark/90" />
+      <Image
+        src={imagePath}
+        placeholder="blur"
+        blurDataURL={blog.image.blurhashDataUrl}
+        alt={blog.title}
+        width={blog.img.width}
+        height={blog.img.height}
+        className="-z-10 h-full w-full rounded-3xl object-cover object-center"
+      />
+      <div className="z-0 flex w-3/4 flex-col items-start justify-center p-16 text-light">
+        <Tag link={`/categories/${blog.tags}`} name={blog.tags} />
+        <Link href={blog.url} className="mt-6">
+          <h1 className="text-4xl font-bold capitalize">
+            <span className="bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_6px]">
+              {blog.title}
+            </span>
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default BlogLayoutOne;
--- src/components/home/coverSection.js
+++ src/components/home/coverSection.js
@@ -11,21 +11,25 @@ const CoverSection = ({ blogs }) => {
   return (
     <div className="inline-block w-full">
       <article className="relative mx-10 flex h-[85vh] flex-col items-start justify-end">
-        <div className="rounded-3x1 absolute bottom-0 left-0 right-0 top-0 -z-10 h-full bg-gradient-to-b from-transparent from-0% to-dark" />
+        <div className="rounded-3x1 absolute bottom-0 left-0 right-0 top-0 -z-10 h-full bg-gradient-to-b from-transparent from-0% to-dark/90" />
         <Image
           src={imagePath}
           placeholder="blur"
           blurDataURL={blog.image.blurhashDataUrl}
           alt={blog.title}
           fill
-          className="w=full rounded-3x1 h-full object-cover object-center"
+          className="w-full rounded-3xl h-full object-cover object-center -z-10"
         />
         <div className="z-0 flex w-3/4 flex-col items-start justify-center p-16 text-light">
           <Tag link={`/categories/${blog.tags}`} name={blog.tags} />
           <Link href={blog.url} className="mt-6">
-            <h1 className="text-4xl font-bold capitalize"><span className="bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">{blog.title}</span></h1>
+            <h1 className="text-4xl font-bold capitalize">
+              <span className="bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_6px]">
+                {blog.title}
+              </span>
+            </h1>
           </Link>
-          <p>{blog.description}</p>
+          <p className="inline-block mt-4 text-xl font-in">{blog.description}</p>
         </div>
       </article>
     </div>
