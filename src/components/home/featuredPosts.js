986bcf508616eff9711cb4cb656cce7e9bbe9666
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-07-16 18:47:02 +0200

    Feat/blog layout two (#7)
    * create and add component
    
    create blogLayoutTwo copomonent and import it into featuredPosts
    
    * component with logic
    
    add component with logic and styling
    
    * blog data
    
    add blog data to map an array
    
    * add date to post
    
    add publish date to each post
    
    * change date text
    
    for gray
    
    ---------
    
    Co-authored-by: Szymon Taranczewski <113885422+mystertaran@users.noreply.github.com>

--- src/components/home/featuredPosts.js
+++ src/components/home/featuredPosts.js
@@ -1,24 +1,29 @@
 import { sortBlogs } from "@/utils";
 import React from "react";
-import BlogLayoutOne from "@/components/blog/blogLayoutOne"
+import BlogLayoutOne from "@/components/blog/blogLayoutOne";
+import BlogLayoutTwo from "@/components/blog/blogLayoutTwo";
 
 const FeaturedPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
-  return <section className="w-full mt-32 px-32 flex flex-col items-center justify-center">
-    <h2 className="w-full inline-block font-bold capitalize text-4xl">Featured Posts</h2>
+  return (
+    <section className="mt-32 flex w-full flex-col items-center justify-center px-32">
+      <h2 className="inline-block w-full text-4xl font-bold capitalize">
+        Featured Posts
+      </h2>
 
-    <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-16">
-        <article className="col-span-1 row-span-2 relative">
-            <BlogLayoutOne blog={sortedBlogs[2]}/>
+      <div className="mt-16 grid grid-cols-2 grid-rows-2 gap-6">
+        <article className="relative col-span-1 row-span-2">
+          <BlogLayoutOne blog={sortedBlogs[2]} />
         </article>
-        <article className="col-span-1 row-span-1 relative">
-            blog layout 2
+        <article className="relative col-span-1 row-span-1">
+          <BlogLayoutTwo blog={sortedBlogs[3]} />
         </article>
-        <article className="col-span-1 row-span-1 relative">
-            blog layout 2
+        <article className="relative col-span-1 row-span-1">
+          <BlogLayoutTwo blog={sortedBlogs[4]} />
         </article>
-    </div>
-  </section>;
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedPosts;
